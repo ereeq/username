1865f3e2190b54ae9d319229ff56e818f99cf20a
Eric Lim <jilim@chapman.edu>
2018-11-10 19:08:15 -0800

    fix

--- username.sh
+++ username.sh
@@ -2,20 +2,11 @@
 # username.sh
 echo "Username must be 3~12 characters, ONLY lowercase, digits or underscore. "
 echo "Username must start with a lower case letter."
-while true
+read username
+while echo $username | egrep -v "^[a-z0-9_]{3-12}$" > /dev/null 2>&1
 do
-	echo "Enter Username: "
+	echo "You must enter a valid ZIP code - five digits only!"
+	echo "Enter a five-digit ZIP code: "
 	read username
-	size=${#username}
-	firstletter=${username:0:1}
-	if [$size -gt 2 $size -le 13]]; then
-		if [[$firstletter =~ [a-z]]]; then
-			if [[$username =~ [a-z0-9_]]]; then
-				$mybool = $false
-				break
-			fi
-		fi
-	fi
-	echo "illegal username"
 done
 echo "Thank you"
